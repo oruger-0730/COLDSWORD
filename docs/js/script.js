8adc2302bed0eb38f850775821c30bcbcda7e1ff
JUN-SUZU <jun2006.master@gmail.com>
2024-09-29 08:47:32 +0900

    Refactor file paths for game and login pages

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,7 +1,7 @@
 let username = localStorage.getItem('username');
 if (username) {
     // iconimage url
-    document.getElementById('userIcon').src = "../img/user-icon/" + username + ".png";
+    document.getElementById('userIcon').src = "./../img/user-icon/" + username + ".png";
     document.getElementById('userName').innerText = username;
     document.getElementById('loginButton').style.display = 'none';
     document.getElementById('registerButton').style.display = 'none';

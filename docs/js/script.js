c79c4775133bec6837c50585e3c7944e32a29b32
JUN-SUZU <jun2006.master@gmail.com>
2024-09-28 23:14:40 +0900

    style

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,9 +1,3 @@
-// ページロード時に画面の向きを確認する
-window.addEventListener("orientationchange", function () {
-    if (window.orientation === 0 || window.orientation === 180 || (window.innerHeight > window.innerWidth)) {
-        alert("横向きにしてください！このままゲームを進めるとエラーが発生します。 error code **1-001**");
-    }
-});
 let username = localStorage.getItem('username');
 if (username) {
     // iconimage url

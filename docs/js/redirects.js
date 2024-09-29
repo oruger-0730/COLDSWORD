def456beca4fd7e2c25e2b15b8f09e85a54ff17f
Yamatomato0105 <yamatomato0105@gmail.com>
2024-09-29 15:12:35 +0900

    Create "redirects.js"

---
+++ docs/js/redirects.js
@@ -0,0 +1,14 @@
+// ログインページへの遷移
+function goToLogin() {
+ window.location.href = "/login";
+ }
+   
+// 新規登録ページへの遷移
+function goToSignup() {
+    window.location.href = "/signup";
+}
+       
+// ゲームプレイページに遷移
+function goToPlay() {
+    window.location.href = "/game";
+}
